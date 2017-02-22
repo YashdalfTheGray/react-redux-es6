91a8b2f9a12f465990d8d06105ee2ea9128c5735
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-02-21 20:30:17 -0800

    fixed the webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,18 +1,16 @@
-import webpack from 'webpack';
-import path from 'path';
+const webpack = require('webpack');
+const path = require('path');
 
-export default {
-    debug: true,
+module.exports = {
     devtool: 'inline-source-map',
-    noInfo: false,
     entry: [
-        'eventsource-polyfill', // necessary for hot reloading with IE
-        'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
+        'eventsource-polyfill',
+        'webpack-hot-middleware/client?reload=true',
         path.resolve(__dirname, 'src/index')
     ],
     target: 'web',
     output: {
-        path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
+        path: __dirname + '/dist',
         publicPath: '/',
         filename: 'bundle.js'
     },
@@ -28,27 +26,51 @@ export default {
             {
                 test: /\.js$/,
                 include: path.join(__dirname, 'src'),
-                loaders: ['babel']
+                use: ['babel-loader']
             },
             {
                 test: /(\.css)$/,
-                loaders: ['style', 'css']
+                use: ['style-loader', 'css-loader']
             },
             {
                 test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'file'
+                use: ['file-loader']
             },
             {
                 test: /\.(woff|woff2)$/,
-                loader: 'url?prefix=font/&limit=5000'
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            prefix: 'font/',
+                            limit: '5000'
+                        }
+                    }
+                ]
             },
             {
                 test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=application/octet-stream'
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: '10000',
+                            mimetype: 'application/octet-stream'
+                        }
+                    }
+                ]
             },
             {
                 test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=image/svg+xml'
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: '10000',
+                            mimetype: 'image/svg+xml'
+                        }
+                    }
+                ]
             }
         ]
     }
