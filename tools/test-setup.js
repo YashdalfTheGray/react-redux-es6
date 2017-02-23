8b0f0fba59a5a891650d3841234689a6ffcff14f
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-02-22 23:00:09 -0800

    added some test scafolding, still working on why it's not in tape

---
+++ tools/test-setup.js
@@ -0,0 +1,24 @@
+process.env.NODE_ENV = 'test';
+
+require('babel-register')();
+
+require.extensions['.css'] = () => null;
+require.extensions['.png'] = () => null;
+require.extensions['.jpg'] = () => null;
+
+const jsdom = require('jsdom').jsdom;
+const exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+
+Object.keys(document.defaultView).forEach((property) => {
+    if (typeof global[property] === 'undefined') {
+        exposedProperties.push(property);
+        global[property] = document.defaultView[property];
+    }
+});
+
+global.navigator = { userAgent: 'node.js' };
+
+documentRef = document;  //eslint-disable-line no-undef
