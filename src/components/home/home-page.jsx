43b699636f94bb8a3ab116f25ed79333cf24c94e
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-02-24 20:48:29 -0800

    added a couple of other components to the project

---
+++ src/components/home/home-page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+    render() {
+        return (
+            <div>
+                <h1>Home</h1>
+                <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
